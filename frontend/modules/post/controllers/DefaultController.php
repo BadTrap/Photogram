60e5682c179a976fbc3e67faf53d79a33628ada5
BadTrap <iudut@bk.ru>
2018-09-18 14:26:00 +0300

    Like and Unlike functionality

--- frontend/modules/post/controllers/DefaultController.php
+++ frontend/modules/post/controllers/DefaultController.php
@@ -2,56 +2,114 @@
 
 namespace frontend\modules\post\controllers;
 
-use frontend\models\Post;
 use Yii;
-use frontend\modules\post\models\forms\PostForm;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
+use yii\web\Response;
 use yii\web\UploadedFile;
+use frontend\models\Post;
+use frontend\modules\post\models\forms\PostForm;
 
 /**
  * Default controller for the `post` module
  */
 class DefaultController extends Controller
 {
+
     /**
-     * Renders the Create view for the module
+     * Renders the create view for the module
      * @return string
      */
     public function actionCreate()
     {
         $model = new PostForm(Yii::$app->user->identity);
 
-        if  ($model->load(Yii::$app->request->post())){
+        if ($model->load(Yii::$app->request->post())) {
 
             $model->picture = UploadedFile::getInstance($model, 'picture');
-            if  ($model->save())  {
-                Yii::$app->session->setFlash("success", "Post created");
+
+            if ($model->save()) {
+
+                Yii::$app->session->setFlash('success', 'Post created!');
                 return $this->goHome();
             }
-
         }
 
-        return $this->render('create',[
+        return $this->render('create', [
             'model' => $model,
         ]);
-
     }
 
+    /**
+     * Renders the create view for the module
+     * @return string
+     */
     public function actionView($id)
     {
-        return $this->render('view',[
-           'post' => $this->findPost($id),
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+
+        return $this->render('view', [
+            'post' => $this->findPost($id),
+            'currentUser' => $currentUser,
         ]);
     }
 
-    public function findPost($id)
+    public function actionLike()
     {
-        if ($user = Post::findOne($id)){
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        $id = Yii::$app->request->post('id');
+        $post = $this->findPost($id);
+
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+
+        $post->like($currentUser);
+
+        return [
+            'success' => true,
+            'likesCount' => $post->countLikes(),
+        ];
+    }
+
+    public function actionUnlike()
+    {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/user/default/login']);
+        }
+
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        $id = Yii::$app->request->post('id');
+
+        /* @var $currentUser User */
+        $currentUser = Yii::$app->user->identity;
+        $post = $this->findPost($id);
+
+        $post->unLike($currentUser);
+
+        return [
+            'success' => true,
+            'likesCount' => $post->countLikes(),
+        ];
+    }
+
+    /**
+     * @param integer $id
+     * @return User
+     * @throws NotFoundHttpException
+     */
+    private function findPost($id)
+    {
+        if ($user = Post::findOne($id)) {
             return $user;
         }
         throw new NotFoundHttpException();
-
     }
+
 
 }
